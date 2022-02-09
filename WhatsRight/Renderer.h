85f32f8b4ea6fa33e4f7138c52eaf32047f39162
Hevano <evanohirabe@gmail.com>
2022-02-09 12:42:04 -0800

    Added Project

---
+++ WhatsRight/Renderer.h
@@ -0,0 +1,30 @@
+//
+//  Copyright © Borna Noureddin. All rights reserved.
+//
+
+#ifndef Renderer_h
+#define Renderer_h
+#import <GLKit/GLKit.h>
+
+@interface Renderer : NSObject
+
+// To give an interface to Swift for variables, they need to be Objective-C properties. You can add additional ones, such as a toggle variable for auto-rotating a cube.
+@property bool isRed;
+@property bool isRotating;
+@property float rotAngle;
+@property float zoom;
+@property bool reset;
+@property GLKVector2 panRotation;
+@property GLKVector2 position;
+
+- (void)setup:(GLKView *)view;
+- (void)loadModels;
+- (void)update;
+- (void)draw:(CGRect)drawRect;
+- (void)rotate:(float)xAxis secondAxis:(float)yAxis thirdAxis:(float)zAxis;
+- (void)translate:(float)xAxis secondAxis:(float)yAxis thirdAxis:(float)zAxis;
+@end
+
+#endif /* Renderer_h */
+
+
