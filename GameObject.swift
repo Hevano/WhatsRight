cd97d34217b1a871ac6ca593cbc8e136cc85b84b
Hevano <evanohirabe@gmail.com>
2022-02-16 12:40:44 -0800

    Removed swift game object

--- GameObject.swift
+++ GameObject.swift
@@ -4,27 +4,5 @@
 //
 //  Created by Evano Hirabe on 2022-02-16.
 //
-/*
-import GLKit;
 
-class GameObject {
-    GLuint vbo; //VBO is stored on the gpu and can be referenced by an id
-    GLenum texture; //Textures are loaded in slots with id GL_TEXTURE0 - GL_TEXTURE15
-    
-    GLKVector3 position;
-    GLKVector3 scale;
-    GLKQuaternion rotation;
-    
-    init(transform: GLKMatrix4, model: String, Texture: String){
-        //Load Model as list of vertices
-        //Generate buffer
-        //Bind buffer
-        //Load buffer with model data
-        //Setup texture
-        
-    }
-    
-    func modelMatrix() -> GLKMatrix4 {
-        
-    }
-}*/
+
