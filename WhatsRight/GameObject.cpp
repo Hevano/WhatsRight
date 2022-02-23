aa5f166fdd3f69c7b4da86fbd6d024701eb8765b
Snow <snow.nh106@gmail.com>
2022-02-23 12:55:30 -0800

    Small code review

--- WhatsRight/GameObject.cpp
+++ WhatsRight/GameObject.cpp
@@ -6,36 +6,25 @@
 //
 
 #include "GameObject.hpp"
-#include "glm/gtc/quaternion.hpp"
-#include "glm/gtx/quaternion.hpp"
 
 GameObject::GameObject(int numIndices, float *vertices, float *normals, float *texCoords, int *indices) {
-    position = glm::vec3(0.0f);
-    scale = glm::vec3(1.0f);
-    rotation = glm::quat(glm::vec3(0.0f));
+    m_position = glm::vec3(0.0f);
+    m_scale = glm::vec3(1.0f);
+    m_rotation = glm::quat(glm::vec3(0.0f));
     
     updateModelMatrix();
     
-    this->numIndices = numIndices;
-    this->vertices = vertices;
-    this->normals = normals;
-    this->texCoords = texCoords;
-    this->indices = indices;
+    m_numIndices = numIndices;
+    m_vertices = vertices;
+    m_normals = normals;
+    m_texCoords = texCoords;
+    m_indices = indices;
 }
 
 glm::mat4 GameObject::updateModelMatrix(){
-    glm::mat4 model = glm::translate(glm::mat4(1.0f), position)
-    * glm::toMat4(rotation);
-    glm::scale(model, scale);
-    modelMatrix = model;
+    glm::mat4 model = glm::translate(glm::mat4(1.0f), m_position)
+    * glm::toMat4(m_rotation);
+    glm::scale(model, m_scale);
+    m_modelMatrix = model;
     return model;
 }
-void GameObject::setScale(glm::vec3 newScale){
-    scale = newScale;
-};
-void GameObject::setRotation(glm::quat newRotation){
-    rotation = newRotation;
-};
-void GameObject::setPosition(glm::vec3 newPosition){
-    position = newPosition;
-};
--- WhatsRight/GameObject.hpp
+++ WhatsRight/GameObject.hpp
@@ -18,24 +18,26 @@
 class GameObject
 {
 public:
-    float *vertices;
-    // ### add additional vertex data (e.g., vertex normals, texture coordinates, etc.) here
-    float *normals, *texCoords;
-    int *indices, numIndices;
-
-    glm::vec3 position;
-    glm::vec3 scale;
-    glm::quat rotation;
-    glm::mat4 modelMatrix;
-    
+  
     GameObject(int numIndices, float *vertices, float *normals, float *texCoords, int *indices);
 
-    void setScale(glm::vec3 scale);
-    void setRotation(glm::quat rotation);
-    void setPosition(glm::vec3 position);
+    void setScale(glm::vec3 scale) { m_scale = std::move(scale); }
+    void setRotation(glm::quat rotation) { m_rotation = std::move(rotation); }
+    void setPosition(glm::vec3 position) { m_position = std::move(position); }
+    
     
 private:
     glm::mat4 updateModelMatrix();
+    
+    float *m_vertices;
+    // ### add additional vertex data (e.g., vertex normals, texture coordinates, etc.) here
+    float *m_normals, *m_texCoords;
+    int *m_indices, m_numIndices;
+
+    glm::vec3 m_position;
+    glm::vec3 m_scale;
+    glm::quat m_rotation;
+    glm::mat4 m_modelMatrix;
 };
 
 #endif /* GameObject_hpp */
